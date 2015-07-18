7b0b887b72797882bd06216a5cf7a08d93ffd658
Emadmohamad <emad90mohamad@gmail.com>
2015-07-18 15:01:03 -0700

    Initial commit

---
+++ StartScene.swift
@@ -0,0 +1,16 @@
+//
+//  StartScene.swift
+//  ColorTap
+//
+//  Created by Emad Mohamad on 7/18/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class StartScene: CCScene {
+   
+    weak var colorTapLabel : CCLabelTTF!
+    
+    
+}
